4593d4b54e0392ef40ea0eb43df942baec324f95
phi <phi1618jp@gmail.com>
2018-07-20 18:52:23 +0900

    fix chapter02

---
+++ go_4beginners/chapter02/sample0207.go
@@ -0,0 +1,20 @@
+// break，continue
+package main
+
+import(
+	"fmt"
+)
+
+func main() {
+	n := 0
+	for {
+		n++
+		if n > 10 {
+			break
+		}
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
