25442d17f9670ebd52486ba7aae48edd4adcc520
phi <phi1618jp@gmail.com>
2018-07-20 17:49:48 +0900

    fix go for beginners

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # go-learning
+
+
+- [はじめてのGo](/go_4beginners) ... http://gihyo.jp/dev/feature/01/go_4beginners/
---
+++ go_4beginners/chapter01/myproject/main.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+	"fmt"
+	"gosample"
+)
+
+func main() {
+	fmt.Println("hello world")
+	fmt.Println(gosample.Message)
+}
